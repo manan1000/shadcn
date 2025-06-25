4912ce5c920cf05c3c179dfcb0b2f533fc47a0a8
manan1000 <mananhemani29@gmail.com>
2025-06-25 19:54:12 +0530

    area chart

--- src/components/AppAreaChart.tsx
+++ src/components/AppAreaChart.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts"
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts"
 
 import { ChartConfig, ChartContainer } from "@/components/ui/chart"
 import { ChartTooltip, ChartTooltipContent, ChartLegend, ChartLegendContent } from "@/components/ui/chart"
@@ -17,43 +17,83 @@ const chartData = [
 const chartConfig = {
     desktop: {
         label: "Desktop",
-        color: "var(--chart-1)",
+        color: "var(--chart-4)",
     },
     mobile: {
         label: "Mobile",
-        color: "var(--chart-2)",
+        color: "var(--chart-5)",
     },
 } satisfies ChartConfig
 
-export function AppBarChart() {
+export function AppAreaChart() {
     return (
 
         <div className="">
-            <h1 className="text-lg font-medium mb-6">Total Revenue</h1>
+            <h1 className="text-lg font-medium mb-6">Visitors</h1>
             <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-                <BarChart accessibilityLayer data={chartData}>
+                <AreaChart
+                    accessibilityLayer
+                    data={chartData}
+                    margin={{
+                        left: 12,
+                        right: 12,
+                    }}
+                >
                     <CartesianGrid vertical={false} />
                     <XAxis
                         dataKey="month"
                         tickLine={false}
-                        tickMargin={10}
                         axisLine={false}
+                        tickMargin={8}
                         tickFormatter={(value) => String(value).slice(0, 3)}
                     />
-
-                    <YAxis
-                        tickLine={false}
-                        tickMargin={10}
-                        axisLine={false}
+                    <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+                    <defs>
+                        <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                            <stop
+                                offset="5%"
+                                stopColor="var(--color-desktop)"
+                                stopOpacity={0.8}
+                            />
+                            <stop
+                                offset="95%"
+                                stopColor="var(--color-desktop)"
+                                stopOpacity={0.1}
+                            />
+                        </linearGradient>
+                        <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                            <stop
+                                offset="5%"
+                                stopColor="var(--color-mobile)"
+                                stopOpacity={0.8}
+                            />
+                            <stop
+                                offset="95%"
+                                stopColor="var(--color-mobile)"
+                                stopOpacity={0.1}
+                            />
+                        </linearGradient>
+                    </defs>
+                    <Area
+                        dataKey="mobile"
+                        type="natural"
+                        fill="url(#fillMobile)"
+                        fillOpacity={0.4}
+                        stroke="var(--color-mobile)"
+                        stackId="a"
                     />
-                    <ChartTooltip content={<ChartTooltipContent />} />
-                    <ChartLegend content={<ChartLegendContent />} />
-                    <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-                    <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
-                </BarChart>
+                    <Area
+                        dataKey="desktop"
+                        type="natural"
+                        fill="url(#fillDesktop)"
+                        fillOpacity={0.4}
+                        stroke="var(--color-desktop)"
+                        stackId="a"
+                    />
+                </AreaChart>
             </ChartContainer>
         </div>
     )
 }
 
-export default AppBarChart;
+export default AppAreaChart;
